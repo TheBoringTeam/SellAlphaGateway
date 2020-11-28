bfab44e92eb06ee4a8ad49699eb4a3cb7c9227db
HolkerDev <nick.penkovskiy@gmail.com>
2020-11-28 17:28:59 +0100

    add init project

---
+++ src/main/java/eu/sell/alphagateway/AlphagatewayApplication.java
@@ -0,0 +1,13 @@
+package eu.sell.alphagateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphagatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlphagatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/eu/sell/alphagateway/AlphagatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.sell.alphagateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphagatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
