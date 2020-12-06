d748a0db829932a37a02a6c97c00b6c514b88795
HolkerDev <nick.penkovskiy@gmail.com>
2020-12-06 21:08:21 +0100

    add security

---
+++ src/main/java/eu/sell/alphagateway/security/SecurityConfig.java
@@ -0,0 +1,38 @@
+package eu.sell.alphagateway.security;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private static final Logger log = LoggerFactory.getLogger(SecurityConfig.class);
+
+    private Environment env;
+
+    @Autowired
+    SecurityConfig(Environment env) {
+        this.env = env;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.POST, env.getProperty("url.register")).permitAll()
+                .antMatchers(HttpMethod.POST, env.getProperty("url.login")).permitAll()
+                .anyRequest().authenticated();
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+}
